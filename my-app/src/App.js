63776d27783fa1775914864999e4d2880f4a127d
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-03-04 17:37:30 +0100

    i

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -24,10 +24,15 @@ class App extends React.Component {
     console.log("fetching...");
     fetch("http://127.0.0.1:8000/todo/task-list/")
       .then((response) => response.json())
-      .then((data) => console.log('Data:', data));
+      .then((data) =>
+        this.setState({
+          todoList: data,
+        })
+      );
   }
 
   render() {
+    var tasks = this.state.todoList;
     return (
       <div className="container">
         <div id="task-container">
@@ -55,7 +60,27 @@ class App extends React.Component {
               </div>
             </form>
           </div>
-          <div id="list-wrapper"></div>
+          <div id="list-wrapper">
+            {tasks.map(function (task, index) {
+              return (
+                <div key={index} className="task-wrapper flex-wrapper">
+                  <div style={{ flex: 7 }}>
+                    <span>{task.title}</span>
+                  </div>
+                  <div style={{ flex: 1 }}>
+                    <button className="btn btn-sm btn-outline-info">
+                      Edit
+                    </button>
+                  </div>
+                  <div style={{ flex: 1 }}>
+                    <button className="btn btn-sm btn-outline-dark delete">
+                    {/* <ion-icon name="checkmark-outline"></ion-icon> */}-
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     );
