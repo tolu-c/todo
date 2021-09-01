cc53e18afda31a6819181a5a1a5a0f4f277eaeaf
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-09-01 21:53:45 +0100

    added cors headers and calling the drf api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList: [],
+      activeItem: {
+        id: null,
+        title: "",
+        completed: false,
+      },
+      editing: false,
+    };
+
+    this.fetchTasks = this.fetchTasks.bind(this);
+  }
+
+  componentWillMount() {
+    this.fetchTasks();
+  }
+
+  fetchTasks() {
+    console.log("fetching...");
+
+    fetch("http://127.0.0.1:8000/api/task-list/")
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          todoList: data,
+        })
+      );
+  }
+
+  render() {
+    let tasks = this.state.todoList
+    return (
+      <div className="container">
+        <div id="task-container">
+          <div id="form-wrapper">
+            <form id="form">
+              <div className="flex-wrapper">
+                <div style={{ flex: 6 }}>
+                  <input
+                    className="form-control"
+                    id="title"
+                    type="text"
+                    placeholder="Add Task"
+                    name="title"
+                  />
+                </div>
+
+                <div style={{ flex: 1 }}>
+                  <input id="submit" class="btn" type="submit" />
+                </div>
+              </div>
+            </form>
+          </div>
+
+          <div id="list-wrapper">
+            {tasks.map(function(task, index){
+              return (
+                <div key={index} className='task-wrapper flex-wrapper'>
+                  <span>{task.title}</span>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
--- todo/settings.py
+++ todo/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
