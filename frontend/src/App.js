bda3ea936ee1dea8329f5fe7a6faa6e6e5abcdce
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-09-02 10:43:41 +0100

    added frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,206 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList: [],
+      activeItem: {
+        id: null,
+        title: "",
+        completed: false,
+      },
+      editing: false,
+    };
+
+    this.fetchTasks = this.fetchTasks.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.getCookie = this.getCookie.bind(this);
+    this.startEdit = this.startEdit.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.completed = this.completed.bind(this);
+  }
+
+  getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+      let cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        if (cookie.substring(0, name.length + 1) === name + "=") {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+
+  componentWillMount() {
+    this.fetchTasks();
+  }
+
+  fetchTasks() {
+    console.log("fetching...");
+
+    fetch("http://127.0.0.1:8000/api/task-list/")
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          todoList: data,
+        })
+      );
+  }
+
+  handleChange(e) {
+    let value = e.target.value;
+
+    this.setState({
+      activeItem: {
+        ...this.state.activeItem,
+        title: value,
+      },
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log("Item:", this.state.activeItem);
+    let csrftoken = this.getCookie("csrftoken");
+
+    let url = "http://127.0.0.1:8000/api/task-create/";
+
+    if (this.state.editing === true) {
+      url = `http://127.0.0.1:8000/api/task-update/${this.state.activeItem.id}/`;
+      this.setState({
+        editing: false,
+      });
+    }
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        "X-CSRFToken": csrftoken,
+      },
+      body: JSON.stringify(this.state.activeItem),
+    })
+      .then((response) => {
+        this.fetchTasks();
+        this.setState({
+          activeItem: {
+            id: null,
+            title: "",
+            completed: false,
+          },
+        });
+      })
+      .catch(function (error) {
+        console.log(`Error: ${error}`);
+      });
+  }
+
+  startEdit(task) {
+    this.setState({
+      activeItem: task,
+      editing: true,
+    });
+  }
+
+  deleteItem(task) {
+    let csrftoken = this.getCookie("csrftoken");
+    fetch(`http://127.0.0.1:8000/api/task-delete/${task.id}/`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+        "X-CSRFToken": csrftoken,
+      },
+    }).then((response) => {
+      this.fetchTasks();
+    });
+  }
+
+  completed(task) {
+    task.completed = !task.completed;
+    console.log(`task: ${task.completed}`);
+    let csrftoken = this.getCookie("csrftoken");
+    let url = `http://127.0.0.1:8000/api/task-update/${task.id}/`;
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        "X-CSRFToken": csrftoken,
+      },
+      body: JSON.stringify({ completed: task.completed, title: task.title }),
+    }).then(() => {
+      this.fetchTasks();
+    });
+  }
+
+  render() {
+    let tasks = this.state.todoList;
+    let self = this;
+
+    return (
+      <div className="container">
+        <div id="task-container">
+          <div id="form-wrapper">
+            <form id="form" onSubmit={this.handleSubmit}>
+              <div className="flex-wrapper">
+                <div style={{ flex: 6 }}>
+                  <input
+                    className="form-control"
+                    onChange={this.handleChange}
+                    id="title"
+                    type="text"
+                    placeholder="Add Task"
+                    name="title"
+                    value={this.state.activeItem.title}
+                  />
+                </div>
+
+                <div style={{ flex: 1 }}>
+                  <input id="submit" className="btn" type="submit" />
+                </div>
+              </div>
+            </form>
+          </div>
+
+          <div id="list-wrapper">
+            {tasks.map(function (task, index) {
+              return (
+                <div key={index} className="task-wrapper flex-wrapper">
+                  <div onClick={() => self.completed(task)} style={{ flex: 7 }}>
+                    {task.completed === false ? (
+                      <span>{task.title}</span>
+                    ) : (
+                      <strike>{task.title}</strike>
+                    )}
+                  </div>
+                  <div style={{ flex: 1 }}>
+                    <button
+                      onClick={() => self.startEdit(task)}
+                      className="btn btn-sm btn-outline-info"
+                    >
+                      Edit
+                    </button>
+                  </div>
+                  <div style={{ flex: 1 }}>
+                    <button
+                      onClick={() => self.deleteItem(task)}
+                      className="btn btn-sm btn-outline-dark delete"
+                    >
+                      -
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
