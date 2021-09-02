ce2ef8d08e90abd4342314c7f9d863106f171525
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-09-02 09:32:22 +0100

    not building

--- todo/settings.py
+++ todo/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build/static')
-]
+STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "frontend/build/static"]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
