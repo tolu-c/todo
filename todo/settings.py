7750cbc7c51dcaee83dbeea23a23dcaae54b0e78
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-09-03 01:51:31 +0100

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR / "frontend/builder/static"]
+STATICFILES_DIRS = [BASE_DIR / "frontend/builder/static"]
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'frontend/builder/static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
