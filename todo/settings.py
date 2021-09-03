e70a477abb73079f51c1373888f51c90fa57f176
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-09-03 01:14:51 +0100

    removed staticfile dirs

--- todo/settings.py
+++ todo/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "frontend/builder/static"]
+# STATICFILES_DIRS = [BASE_DIR / "frontend/builder/static"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
