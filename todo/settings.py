077fd8442ba59815ac48049caf9d6dcf415e8f55
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-11-03 15:49:27 +0100

    Changed cors settings

--- todo/settings.py
+++ todo/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'frontend/builder/static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
+    "*",
 ]
 
 django_heroku.settings(locals())
